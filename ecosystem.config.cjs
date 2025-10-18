5df3f377277d470bd0dbb2c9b853123174c4c8dc
Skylord2121 <skylord2121@users.noreply.github.com>
2025-10-18 17:28:20 +0000

    fix: Add CLOUDFLARE_API_TOKEN to PM2 environment

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
       args: 'wrangler pages dev dist --ip 0.0.0.0 --port 3000',
       env: {
         NODE_ENV: 'development',
-        PORT: 3000
+        PORT: 3000,
+        CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN
       },
       watch: false,
       instances: 1,
